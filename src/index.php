2da948fd75782f6f0890ee979bd3f41606cab018
Petr Chromec <mortal_flesh@seznam.cz>
2018-05-04 00:37:46 +0200

    Throwing my exception by failure handler of my own.

--- src/index.php
+++ src/index.php
@@ -8,15 +8,27 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // application
 use MF\ErrorHandling\Email\Entity\Email;
 
-$emailFacade = new \MF\ErrorHandling\Email\Facade\EmailFacade();
-$email = $emailFacade->createEmail($emailValue, function(string $emailValue): ?Email {
-    // I don't care...
-    return null;
-});
+class MyException extends \Exception
+{
 
-$result = $email
-    ? $email->getValue()
-    : 'Invalid';
+    public function __construct(string $invalidEmailValue)
+    {
+        parent::__construct(sprintf('Email "%s" is not valid.', $invalidEmailValue));
+    }
+}
+
+$emailFacade = new \MF\ErrorHandling\Email\Facade\EmailFacade();
+try {
+    $email = $emailFacade->createEmail($emailValue, function (string $emailValue): ?Email {
+        throw new MyException($emailValue);
+    });
+
+    $result = $email
+        ? $email->getValue()
+        : 'Invalid';
+} catch (MyException $e) {
+    $result = $e->getMessage();
+}
 
 // output
 echo sprintf('Value "%s" is email "%s".' . PHP_EOL, $emailValue, $result);
