8bd69af348f58230f94f90482c6cb5cc66d922bf
Petr Chromec <mortal_flesh@seznam.cz>
2018-05-04 00:51:45 +0200

    Using InvalidEmail implementation

--- src/index.php
+++ src/index.php
@@ -8,15 +8,22 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // application
 use MF\ErrorHandling\Email\Entity\Email;
 
+class InvalidEmail extends \MF\ErrorHandling\Email\Entity\Email
+{
+    public function __construct()
+    {
+        parent::__construct('Invalid');
+    }
+}
+
 $emailFacade = new \MF\ErrorHandling\Email\Facade\EmailFacade();
-$email = $emailFacade->createEmail($emailValue, function(string $emailValue): ?Email {
-    // I don't care...
-    return null;
+$email = $emailFacade->createEmail($emailValue, function (string $emailValue): ?Email {
+    return new InvalidEmail();
 });
 
 $result = $email
     ? $email->getValue()
-    : 'Invalid';
+    : 'Invalid - but this should never happened.';
 
 // output
 echo sprintf('Value "%s" is email "%s".' . PHP_EOL, $emailValue, $result);
